e4f2c07f1a627ab7dbda6199036eda87c2db6c7f
herd <herdianwinarno@gmail.com>
2019-01-12 20:40:34 +0700

    Basic structures and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeLayoutComponent } from './Layouts/home-layout/home-layout.component';
+import { HomeComponent } from './Components/home/home.component';
+import { MovieComponent } from './Components/movie/movie.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeLayoutComponent,
+    children: [
+      {
+        path: "",
+        component: HomeComponent,
+      },
+      {
+        path: "movie",
+        component: MovieComponent,
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { HomeComponent } from './Components/home/home.component';
+import { MovieComponent } from './Components/movie/movie.component';
+import { HomeLayoutComponent } from './Layouts/home-layout/home-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    MovieComponent,
+    HomeLayoutComponent
   ],
   imports: [
     BrowserModule,
